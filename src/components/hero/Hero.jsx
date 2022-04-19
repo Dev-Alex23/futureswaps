88eaa8c23d9d0511cc45e4390db19a0d2cb16109
Dev-Alex23 <alexhinds1@outlook.com>
2022-04-19 16:12:28 +0100

    Hero Images needs to be fixed

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -6,43 +6,47 @@ import purple from "../../assets/images/Graphic-Purple-Wave.png";
 
 const Hero = () => {
   return (
-    <>
-      <div className='hero'>
-        <div className='hero__content'>
-          <div className='hero__content__info-container'>
-            <p>Built on ETH. Powerd by you</p>
-            <div className='hero__content__info-container__heading'>
-              <h1>The Future Of Perpetuals Is Here</h1>
+    <div className='hero'>
+      <div className='hero__content'>
+        <div className='hero__content__info-container'>
+          <p>Built on ETH. Powerd by you</p>
+          <div className='hero__content__info-container__heading'>
+            <h1>The Future Of Perpetuals Is Here</h1>
+          </div>
+          <div className='hero__content__info-container__sub-heading'>
+            <p>
+              Leverage up to 20x on a procols trusted with billions for its price
+              execution, super low fees and flawless security record.
+            </p>
+          </div>
+          <div className='hero__content__cta'>
+            <button id='fill'>Start Trade</button>
+            <button id='outline'>Add Funds</button>
+          </div>
+          <div className='hero__content__trade'>
+            <div className='hero__content__trade__alltime'>
+              <p>All time trade</p>
+              <p>£1,000,000,000</p>
             </div>
-            <div className='hero__content__info-container__sub-heading'>
-              <p>
-                Leverage up to 20x on a procols trusted with billions for its price
-                execution, super low fees and flawless security record.
-              </p>
-            </div>
-            <div className='hero__content__cta'>
-              <button id='fill'>Start Trade</button>
-              <button id='outline'>Add Funds</button>
-            </div>
-            <div className='hero__content__trade'>
-              <div className='hero__content__trade__alltime'>
-                <p>All time trade</p>
-                <p>£1,000,000,000</p>
-              </div>
-              <div className='hero__content__trade__alltime'>
-                <p>Total Trade</p>
-                <p>100,000</p>
-              </div>
+            <div className='hero__content__trade__alltime'>
+              <p>Total Trade</p>
+              <p>100,000</p>
             </div>
           </div>
         </div>
       </div>
       <div className='wave-container'>
-        <img src={blue} alt='Graphic-Blue-Wave' />
-        <img src={teal} alt='Graphic-Teal-Wave' />
-        <img src={purple} alt='Graphic-Purple-Wave' />
+        <div className='blue-container'>
+          <img src={blue} alt='Graphic-Blue-Wave' />
+        </div>
+        <div className='teal-container'>
+          <img src={teal} alt='Graphic-Teal-Wave' />
+        </div>
+        <div className='purple-container'>
+          <img src={purple} alt='Graphic-Purple-Wave' />
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
