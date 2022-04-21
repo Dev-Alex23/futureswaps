1e2f9f7e51de1a53ecf67de81c9744fb8de193d1
Dev-Alex23 <alexhinds1@outlook.com>
2022-04-21 19:25:14 +0100

    Coin & coinItem Components

---
+++ src/components/coin/Coin.jsx
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import "./coin.scss";
+import CoinItem from "../coinItem/CoinItem";
+
+const Coin = () => {
+  const [coins, setCoins] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = ({ target }) => {
+    setSearchTerm(target.value);
+    console.log(target.value);
+  };
+
+  const filterCoins = coins.filter((coin) => {
+    return coin.name.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d";
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then(({ data }) => {
+        setCoins(data);
+        console.log(data);
+      })
+      .catch((error) => {
+        alert(error);
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className='form'>
+        <h1>Search Crypto</h1>
+        <input
+          type='text'
+          placeholder='Bitcoin'
+          value={searchTerm}
+          onChange={handleChange}
+        />
+      </div>
+      <div className='coin-head-row'>
+        <div className='coin-head-row__content'>
+          <div className='coin-name'>
+            <h4>Name</h4>
+          </div>
+          <div className='coin-symbol'>
+            <h4>Symbol</h4>
+          </div>
+          <div className='coin-price'>
+            <h4>Price</h4>
+          </div>
+          <div className='change'>
+            <h4>24Hr</h4>
+          </div>
+          <div className='coin-volume'>
+            <h4>Volume</h4>
+          </div>
+          <div className='coin-cap'>
+            <h4>Market Cap</h4>
+          </div>
+        </div>
+
+        {filterCoins.map((coin) => {
+          return (
+            <CoinItem
+              key={coin.id}
+              id={coin.id}
+              image={coin.image}
+              current_price={coin.current_price}
+              price_change_24h={coin.price_change_24h}
+              total_volume={coin.total_volume}
+              market_cap={coin.market_cap}
+            />
+          );
+        })}
+
+        {/* {coins.map((coin) => {
+          return (
+            <CoinItem
+              key={coin.id}
+              id={coin.id}
+              image={coin.image}
+              current_price={coin.current_price}
+              price_change_24h={coin.price_change_24h}
+              total_volume={coin.total_volume}
+              market_cap={coin.market_cap}
+            />
+          );
+        })} */}
+      </div>
+    </>
+  );
+};
+
+export default Coin;
---
+++ src/components/coinItem/CoinItem.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./coinItem.scss";
+
+const CoinItem = ({
+  id,
+  image,
+  current_price,
+  price_change_24h,
+  total_volume,
+  market_cap,
+}) => {
+  return (
+    <div className='coin-row'>
+      <div className='coin-row__content'>
+        <div className='name'>
+          <p>{id}</p>
+        </div>
+        <div className='img'>
+          <img src={image} alt={id} />
+        </div>
+        <div className='price'>
+          <p>${current_price.toLocaleString()}</p>
+        </div>
+        <div className='24hr'>
+          <p>{price_change_24h.toFixed(2)}%</p>
+        </div>
+        <div className='volume'>
+          <p>{total_volume.toLocaleString()}</p>
+        </div>
+        <div className='market-cap'>
+          <p>${market_cap.toLocaleString()}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CoinItem;
