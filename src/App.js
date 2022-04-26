3ca9aa53f12095b4098e90961422258521fecb76
Dev-Alex23 <alexhinds1@outlook.com>
2022-04-26 21:12:36 +0100

    Unnecessary import removed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import Home from "./components/home/Home";
 import "./styles/app.scss";
 
-import { Routes, Route } from "react-router-dom";
-
 function App() {
   return (
     <div className='App'>
