1832599e38a75171aa38a796eb71d98105b44f82
Dev-Alex23 <alexhinds1@outlook.com>
2022-05-02 22:27:39 +0100

    Code refactor for routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Home from "./components/home/Home";
+import CoinPage from "./pages/CoinPage";
 import "./styles/app.scss";
+import Coin from "./components/coin/Coin";
+
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className='App'>
-      <Home />
+      <Routes>
+        <Route path='' element={<Home />} />
+        <Route path='/charts' element={<Coin />} />
+        <Route path='/charts/:id' element={<CoinPage />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import Coin from "./components/coin/Coin";
-import Home from "./components/home/Home";
 
 ReactDOM.render(
   <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<App />} />
-      <Route path='/home' element={<Home />} />
-      <Route path='charts' element={<Coin />} />
-    </Routes>
+    <App />
   </BrowserRouter>,
   document.getElementById("root")
 );
