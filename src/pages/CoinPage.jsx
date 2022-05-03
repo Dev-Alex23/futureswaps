4365ce75322fa75cfcaef10d67dd407175ddb9b1
Dev-Alex23 <alexhinds1@outlook.com>
2022-05-03 17:36:41 +0100

    Coin Info Page component

---
+++ src/pages/CoinPage.jsx
@@ -0,0 +1,106 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import DOMPurify from "dompurify";
+
+import Head from "../components/header/Header";
+import "./coinPage.scss";
+
+const CoinPage = () => {
+  const [coinInfo, setCoinInfo] = useState({});
+
+  let params = useParams();
+
+  const url = `https://api.coingecko.com/api/v3/coins/${params.id}`;
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setCoinInfo(response.data);
+      })
+      .catch((e) => console.error(e));
+  }, [url]);
+  return (
+    <>
+      <Head />
+      <div className='coinInfo'>
+        <div className='coinInfo__content'>
+          <div className='coin-name'>
+            <h1>{coinInfo.name}</h1>
+          </div>
+          <div className='coinInfo__content__coin-info-container'>
+            <div className='coin-image'>
+              <img
+                src='https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579'
+                alt='bitcoin'
+              />
+              <div className='coin-symbol'>
+                <p>{coinInfo.name}</p>
+                <p>({coinInfo.symbol}/USD)</p>
+              </div>
+            </div>
+            <div className='coin-amount'>
+              <h2>
+                $
+                {coinInfo.market_data
+                  ? coinInfo.market_data.current_price.usd.toLocaleString()
+                  : null}
+              </h2>
+            </div>
+          </div>
+          <div className='coinInfo__content__coin-price'>
+            <div className='market-info'>
+              <p>24 Hour Low</p>
+              <p id='red'>
+                $
+                {coinInfo.market_data
+                  ? coinInfo.market_data.low_24h["usd"].toLocaleString()
+                  : null}
+              </p>
+            </div>
+            <div className='market-info'>
+              <p>24 Hour High</p>
+              <p id='green'>
+                $
+                {coinInfo.market_data
+                  ? coinInfo.market_data.high_24h["usd"].toLocaleString()
+                  : null}
+              </p>
+            </div>
+            <div className='market-info'>
+              <p>Market Cap</p>
+              <p>
+                $
+                {coinInfo.market_data
+                  ? coinInfo.market_data.market_cap["usd"].toLocaleString()
+                  : null}
+              </p>
+            </div>
+            <div className='market-info'>
+              <p>Volume</p>
+              <p>
+                $
+                {coinInfo.market_data
+                  ? coinInfo.market_data.total_volume["usd"].toLocaleString()
+                  : null}
+              </p>
+            </div>
+          </div>
+          <div className='coinInfo__content__about'>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(
+                  coinInfo.description ? coinInfo.description["en"] : ""
+                ),
+              }}
+            ></p>
+          </div>
+        </div>
+      </div>
+      {/* <div>{coinInfo.id}</div> */}
+    </>
+  );
+};
+
+export default CoinPage;
