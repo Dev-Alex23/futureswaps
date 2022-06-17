b8d07484b59f074a2e9b6faf07e6d5efa84b717e
Dev-Alex23 <alexhinds1@outlook.com>
2022-06-17 11:54:55 +0100

    Change from ternary operator to optional chaning for more concise coding

--- src/pages/CoinPage.jsx
+++ src/pages/CoinPage.jsx
@@ -29,60 +29,32 @@ const CoinPage = () => {
           </div>
           <div className='coinInfo__content__coin-info-container'>
             <div className='coin-image'>
-              <img
-                src={coinInfo.image ? coinInfo.image.large : null}
-                alt={coinInfo.name}
-              />
+              <img src={coinInfo?.image?.large} alt={coinInfo.name} />
               <div className='coin-symbol'>
                 <p>{coinInfo.name}</p>
                 <p>({coinInfo.symbol}/USD)</p>
               </div>
             </div>
             <div className='coin-amount'>
-              <h2>
-                $
-                {coinInfo.market_data
-                  ? coinInfo.market_data.current_price.usd.toLocaleString()
-                  : null}
-              </h2>
+              <h2>${coinInfo?.market_data?.current_price?.usd.toLocaleString()}</h2>
             </div>
           </div>
           <div className='coinInfo__content__coin-price'>
             <div className='market-info'>
               <p>24 Hour Low</p>
-              <p id='red'>
-                $
-                {coinInfo.market_data
-                  ? coinInfo.market_data.low_24h["usd"].toLocaleString()
-                  : null}
-              </p>
+              <p id='red'>${coinInfo?.market_data?.low_24h["usd"].toLocaleString()}</p>
             </div>
             <div className='market-info'>
               <p>24 Hour High</p>
-              <p id='green'>
-                $
-                {coinInfo.market_data
-                  ? coinInfo.market_data.high_24h["usd"].toLocaleString()
-                  : null}
-              </p>
+              <p id='green'>${coinInfo?.market_data?.high_24h["usd"].toLocaleString()}</p>
             </div>
             <div className='market-info'>
               <p>Market Cap</p>
-              <p>
-                $
-                {coinInfo.market_data
-                  ? coinInfo.market_data.market_cap["usd"].toLocaleString()
-                  : null}
-              </p>
+              <p>${coinInfo?.market_data?.market_cap["usd"].toLocaleString()}</p>
             </div>
             <div className='market-info'>
               <p>Volume</p>
-              <p>
-                $
-                {coinInfo.market_data
-                  ? coinInfo.market_data.total_volume["usd"].toLocaleString()
-                  : null}
-              </p>
+              <p>${coinInfo?.market_data?.total_volume["usd"].toLocaleString()}</p>
             </div>
           </div>
           <div className='coinInfo__content__about'>
